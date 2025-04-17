44cbacb1453ef11b221517922dd4952f968ea030
saikarthikgoud <b.saikarthikgoud@gmail.com>
2025-04-17 18:50:49 +0530

    added examples

--- dcp1.java
+++ dcp1.java
@@ -1,9 +1,34 @@
-public class dcp1 {
+//variables example code
 
-    public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int result =num1 + num2;
-        System.out.println(result);
+// class dcp1 
+//  {
+//     public static void main(String[] var0) 
+//     {
+//        byte var1 = 10;
+//        byte var2 = 20;
+//        int result = var1 + var2;
+//        System.out.println(result);
+//     }
+//  }
+ 
+
+
+
+//casting example code
+public class dcp1 
+{
+    public static void main(String[] args) 
+    {
+        //byte b = 125;
+       
+        int a = 250;
+        byte k = (byte)a;
+        System.out.println(k);
+
+        float f = 10.5f;
+        int t = (int) f;
+        System.out.println(t);
+        
     }
+
 }

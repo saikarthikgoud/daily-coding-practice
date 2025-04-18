4fb8d505984e753319881f16a0b4bf863590d149
saikarthikgoud <b.saikarthikgoud@gmail.com>
2025-04-18 15:19:39 +0530

    examples

--- dcp1.java
+++ dcp1.java
@@ -4,9 +4,9 @@
 //  {
 //     public static void main(String[] var0) 
 //     {
-//        byte var1 = 10;
-//        byte var2 = 20;
-//        int result = var1 + var2;
+//        int num1 = 10;
+//        int num2 = 20;
+//        int result = num1 + num2;
 //        System.out.println(result);
 //     }
 //  }
@@ -14,21 +14,22 @@
 
 
 
-//casting example code
+//type casting  example code
 public class dcp1 
 {
     public static void main(String[] args) 
     {
-        //byte b = 125;
+        //byte b = 127;
        
-        int a = 250;
+        //int to byte casting
+        int a = 257;
         byte k = (byte)a;
         System.out.println(k);
 
+        //float to int casting
         float f = 10.5f;
         int t = (int) f;
         System.out.println(t);
-        
     }
 
 }
